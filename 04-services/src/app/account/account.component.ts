6477e4f2ccd420bbddeee68d981cf29014e057d6
sesp13 <sespinosarte99@gmail.com>
2022-06-25 18:43:50 -0500

    04-services: add some exercises with services concept

--- 04-services/src/app/account/account.component.ts
+++ 04-services/src/app/account/account.component.ts
@@ -1,18 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { AccountsService } from "../services/account.service";
+import { LoggingService } from "../services/logging.service";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  selector: "app-account",
+  templateUrl: "./account.component.html",
+  styleUrls: ["./account.component.css"],
 })
 export class AccountComponent {
-  @Input() account: {name: string, status: string};
+  @Input() account: { name: string; status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(
+    private accountsService: AccountsService,
+    private loggingService: LoggingService
+  ) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountsService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChange(status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- 04-services/src/app/new-account/new-account.component.ts
+++ 04-services/src/app/new-account/new-account.component.ts
@@ -1,18 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { AccountsService } from "../services/account.service";
+import { LoggingService } from "../services/logging.service";
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  selector: "app-new-account",
+  templateUrl: "./new-account.component.html",
+  styleUrls: ["./new-account.component.css"],
 })
-export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+export class NewAccountComponent implements OnInit, OnDestroy {
+  statusSub?: Subscription;
+
+  constructor(
+    private accountService: AccountsService,
+    private loggingService: LoggingService
+  ) {}
+
+  ngOnInit(): void {
+    this.statusSub = this.accountService.statusUpdated.subscribe(
+      (status: string) => {
+        console.log(`New status from service: ${status}`);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.statusSub?.unsubscribe();
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
