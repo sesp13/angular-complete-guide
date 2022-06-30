07797c927c323bd49f9f98ba4522e1525a903e0c
sesp13 <sespinosarte99@gmail.com>
2022-06-29 23:46:23 -0500

    07-forms-reactive: add form cofig and play with it

--- 07-forms/reactive/src/app/app.component.ts
+++ 07-forms/reactive/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders = ["male", "female"];
+  signUpForm: FormGroup;
+
+  get hobbieControls() {
+    return (this.signUpForm.get('hobbies') as FormArray).controls;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.setUpForm();
+  }
+
+  setUpForm(): void {
+    this.signUpForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [Validators.required]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl("male"),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  onAddHobby(): void {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get("hobbies")).push(control);
+  }
+
+  onSubmit(): void {
+    console.log(this.signUpForm);
+  }
 }
