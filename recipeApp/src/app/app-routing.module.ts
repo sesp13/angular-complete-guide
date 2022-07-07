7125a28155a2706e67b239165d3e33a8a3c7fd7c
sesp13 <sespinosarte99@gmail.com>
2022-07-07 17:25:01 -0500

    recipeApp: add recipes module

--- recipeApp/src/app/app-routing.module.ts
+++ recipeApp/src/app/app-routing.module.ts
@@ -11,25 +11,6 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      {
-        path: ':id',
-        component: RecipeDetailComponent,
-        resolve: [RecipeResolverService],
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        resolve: [RecipeResolverService],
-      },
-    ],
-  },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'auth', component: AuthComponent },
   { path: '**', redirectTo: 'recipes' },
--- recipeApp/src/app/app.module.ts
+++ recipeApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // Modules
 import { AppRoutingModule } from './app-routing.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 // Directives
 import { DropdownDirective } from './shared/directives/dropdown.directive';
@@ -19,12 +20,7 @@ import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 
@@ -37,11 +33,6 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
     HeaderComponent,
     LoadingSpinnerComponent,
     PlaceHolderDirective,
-    RecipesComponent,
-    RecipeEditComponent,
-    RecipeDetailComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
     RecipeStartComponent,
     ShoppingEditComponent,
     ShoppingListComponent,
@@ -52,6 +43,7 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    RecipesModule
   ],
   providers: [
     {
