e95b021b0b77ac53e833968dfa70e9e44c563246
sesp13 <sespinosarte99@gmail.com>
2022-07-08 11:13:08 -0500

    recipesApp: add lazy loading

--- recipeApp/src/app/app.module.ts
+++ recipeApp/src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 // Modules
-import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { RecipesModule } from './recipes/recipes.module';
 
 // Components
 import { AppComponent } from './app.component';
@@ -20,11 +16,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AuthModule,
     AppRoutingModule,
     CoreModule,
-    RecipesModule,
-    ShoppingListModule,
     SharedModule,
   ],
   bootstrap: [AppComponent],
