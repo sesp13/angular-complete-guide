b39fc3de9bf1288258cbe2adda3957916bfd9b9e
sesp13 <sespinosarte99@gmail.com>
2022-08-02 23:50:05 -0500

    13-pwa: migrate project to angular 13

--- 13-pwa/src/app/post/post.component.ts
+++ 13-pwa/src/app/post/post.component.ts
@@ -6,6 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-  @Input() title: string;
-  @Input() content: string;
+  @Input() title?: string;
+  @Input() content?: string;
 }
